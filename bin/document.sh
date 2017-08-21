6f4d3199587fcb2d715cc854e6d694beae6f5744
Ariama Victor (A.K.A. OVAC BOYA) <iamovac@gmail.com>
2017-08-21 06:34:43 +0000

    add autobuild for documentation

--- bin/document.sh
+++ bin/document.sh
@@ -9,7 +9,7 @@ echo ""
 [ -e sami.phar ] && echo "Sami found\r\n" || curl -O http://get.sensiolabs.org/sami.phar | php;
 
 # get couscous if it does not exist.
-[ -e sami.phar ] && echo "Couscous found\r\n" || curl -O http://couscous.io/couscous.phar | php;
+[ -e couscous.phar ] && echo "Couscous found\r\n" || curl -O http://couscous.io/couscous.phar | php;
 
 echo "Generating Documentation\r\n";
 
@@ -20,11 +20,14 @@ php sami.phar update ./sami.config.php -v;
 php couscous.phar generate --target=./build/couscous;
 
 # clone the project and climb into the directory and switch to the gh-pages branch
-git clone http://www.github.com/ovac/hubtel-payment && cd hubtel-payment;
+git clone http://www.github.com/ovac/hubtel-payment;
+
+cd hubtel-payment;
+
 git checkout gh-pages;
 
 # Remove all files from the github pages folder
-shopt -s extglob; 
+shopt -s extglob;
 rm -rf !(*.git);
 
 # Make a directory for the sami generated doc and test coverage
@@ -37,7 +40,7 @@ mv  -v ../build/couscous/* ./;
 mv  -v ../build/sami/* ./__/api;
 
 # copy all files from the coverage generated folder into the __coverage folder
-mv  -v ../build/sami/* ./__/coverage;
+mv  -v ../build/coverage/* ./__/coverage;
 
 # Add all and commit to github if deploy was enabled
 git add --all . && git commit -m 'Documentation Updated' && git push origin gh-pages;
